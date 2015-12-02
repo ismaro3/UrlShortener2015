c0ad3fdf88b9b37397fcebe79a890398cdde63a9
Alberto Álvarez Aldea <alberto.alvarez.aldea@hotmail.com>
2015-12-02 11:53:46 +0100

    Adding fields to User

--- candy-pink/src/main/java/urlshortener2015/candypink/domain/User.java
+++ candy-pink/src/main/java/urlshortener2015/candypink/domain/User.java
@@ -1,22 +1,58 @@
 package urlshortener2015.candypink.domain;
 
 public class User {
-    /* Name of the user */
-	private String name;
+    
+	private String username
+	private String password;
+	private String email;
+	private String role;
+	private String name
 
-	/* Type of the user */
-	private String type;
-
-	public User(String name, String type) {
+	public User(String username, String password, String email, String role, String name) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.role = role;
 		this.name = name;
-		this.type = type;
 	}
 
+	public String getUsername() {
+		return this.name;	
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;	
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;	
+	}
+	
+	public String getEmail() {
+		return this.email;	
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;	
+	}
+
+	public String getRole() {
+		return this.role;
+	}
+	
+	public void setRole(String role) {
+		this.role = role;	
+	}
+	
 	public String getName() {
 		return this.name;	
 	}
-
-	public String getType() {
-		return this.type;
+	
+	public void setName(String name) {
+		this.name = name;	
 	}
 }
