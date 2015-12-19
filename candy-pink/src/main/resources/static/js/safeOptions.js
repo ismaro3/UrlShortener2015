aef504f1b12efa2fc4f2ec620535d4ff1247253c
Santi-7 <santi.gil.begue7@gmail.com>
2015-12-19 11:42:17 +0100

    Safe Options javascript Try 6

--- candy-pink/src/main/resources/static/js/safeOptions.js
+++ candy-pink/src/main/resources/static/js/safeOptions.js
@@ -1,9 +1,9 @@
 function showSafeOptions() {
 	if (document.getElementById("safe").innerHTML == " I want a safe short Url") {
 		document.getElementById("safe").innerHTML = " I don't want a safe short Url";
-		$("#show").show()
+		$("p").show();
 	} else {
 		document.getElementById("safe").innerHTML = " I want a safe short Url";
-		$("#show").hide()
+		$("p").hide();
 	}
 }
