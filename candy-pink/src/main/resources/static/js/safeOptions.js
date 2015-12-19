71864586e5d52bd283e9ecb16395e07cb7aed89b
Santi-7 <santi.gil.begue7@gmail.com>
2015-12-19 10:52:57 +0100

    Safe Options javascript Try 3 Temp 2

--- candy-pink/src/main/resources/static/js/safeOptions.js
+++ candy-pink/src/main/resources/static/js/safeOptions.js
@@ -1,7 +1,7 @@
 function showSafeOptions() {
 	document.write("Hola!");
-	document.getElementById("safe").innerHTML = " I don't want a safe short Url";
-	document.getElementById("safeOptions").innerHTML = 
-		"<input type="text" class="center-block form-control input-lg"
-			title="Which users can access the url" placeholder="Text the users here" name="users"></input>";
+	//document.getElementById("safe").innerHTML = " I don't want a safe short Url";
+	//document.getElementById("safeOptions").innerHTML = 
+	//	"<input type="text" class="center-block form-control input-lg"
+	//		title="Which users can access the url" placeholder="Text the users here" name="users"></input>";
 }
