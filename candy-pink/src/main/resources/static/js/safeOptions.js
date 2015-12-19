660937ed91205087459ed17c68362178e3ba5eef
Santi-7 <santi.gil.begue7@gmail.com>
2015-12-19 10:03:40 +0100

    Safe Options javascript

---
+++ candy-pink/src/main/resources/static/js/safeOptions.js
@@ -0,0 +1,5 @@
+function showSafeOptions() {
+  document.getElementById("safeOptions").innerHTML =
+    <input type="text" class="center-block form-control input-lg"
+							title="Which users can access the url?" placeholder="Text the users here" name="users"></input>;
+}
