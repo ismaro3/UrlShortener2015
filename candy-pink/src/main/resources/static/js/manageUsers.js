df1b66245f6552fa210a9192612d12da4dbd83a9
Santi-7 <santi.gil.begue7@gmail.com>
2015-12-23 19:31:07 +0100

    Manage Users JavaScript Try 2

--- candy-pink/src/main/resources/static/js/manageUsers.js
+++ candy-pink/src/main/resources/static/js/manageUsers.js
@@ -1,15 +1,11 @@
 function showUsers() {
-  List<User> users = (new UserRepositoryImpl()).getAllUsers();
-  String text = "";
-  if (users.isEmpty()) {
-    text = "The are no users using your service" + "<br>";
+  var users = (new UserRepositoryImpl()).getAllUsers();
+  var text = "";
+  if (users.length) {
+    text = "The are no users using your service<br>";
   }
   else {
-    var i = 0;
-    for (; i < users.size(); i++) {
-      text += "Username: " + users[i].getUsername() +
-              " Password: " + users[i].getPassword() + "<br>";
-    }
+    text = fruits.join("<br>");
   }
   document.getElementById("users").innerHTML = text;
 }
