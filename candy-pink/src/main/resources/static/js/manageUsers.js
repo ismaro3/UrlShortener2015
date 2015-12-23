8d10b3e231e9e79eb2a558290746ea8ed5290fdd
Santi-7 <santi.gil.begue7@gmail.com>
2015-12-23 19:33:09 +0100

    Manage Users JavaScript Try 2 Fixed

--- candy-pink/src/main/resources/static/js/manageUsers.js
+++ candy-pink/src/main/resources/static/js/manageUsers.js
@@ -1,7 +1,7 @@
 function showUsers() {
   var users = (new UserRepositoryImpl()).getAllUsers();
   var text = "";
-  if (users.length) {
+  if (users.length == 0) {
     text = "The are no users using your service<br>";
   }
   else {
