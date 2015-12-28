51106e1834234e6a5ad191416449d132b55cebf3
Alberto Álvarez Aldea <alberto.alvarez.aldea@hotmail.com>
2015-12-28 16:24:46 +0100

    Update User.java

--- candy-pink/src/main/java/urlshortener2015/candypink/domain/User.java
+++ candy-pink/src/main/java/urlshortener2015/candypink/domain/User.java
@@ -4,14 +4,15 @@ public class User {
     
 	private String username;
 	private String password;
+	private Boolean enabled;
 	private String email;
-	private String role;
+	private String authority;
 
-	public User(String username, String password, String email, String role) {
+	public User(String username, String password, Boolean enabled, String email, String authority) {
 		this.username = username;
 		this.password = password;
 		this.email = email;
-		this.role = role;
+		this.authority = authority;
 	}
 
 	public String getUsername() {
@@ -30,6 +31,13 @@ public class User {
 		this.password = password;	
 	}
 	
+	public Boolean getEnabled() {
+		return this.enabled;
+	}
+	
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;	
+	}
 	public String getEmail() {
 		return this.email;	
 	}
@@ -38,11 +46,12 @@ public class User {
 		this.email = email;	
 	}
 
-	public String getRole() {
-		return this.role;
+
+	public String getAuthority() {
+		return this.authority;
 	}
 	
-	public void setRole(String role) {
-		this.role = role;	
+	public void setAuthority(String authority) {
+		this.authority = authority;	
 	}
 }
