8d4e22b807d291b30cf1a0873ccded78d29916df
Santi-7 <santi.gil.begue7@gmail.com>
2015-12-30 16:49:15 +0100

    Log ManageUsers

--- candy-pink/src/main/java/urlshortener2015/candypink/web/ManageUsersController.java
+++ candy-pink/src/main/java/urlshortener2015/candypink/web/ManageUsersController.java
@@ -31,6 +31,7 @@ public class ManageUsersController {
   	public void getUsers(Model model, HttpServletResponse response) throws IOException {
 		logger.info("Requested all users info");
     		model.addAttribute("users", repo.getAllUsers());
+    		logger.info("Numero de usuarios: " + repo.getAllUsers().size());
 		response.sendRedirect("manageUsersPage.html");
   	}
 }
