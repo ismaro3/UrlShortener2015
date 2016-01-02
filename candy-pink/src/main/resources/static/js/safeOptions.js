e2bf882b3da54bc5f8490f32a44fba795791c784
Santi-7 <santi.gil.begue7@gmail.com>
2016-01-02 14:16:22 +0100

    SafeOptions return to initial value v3

--- candy-pink/src/main/resources/static/js/safeOptions.js
+++ candy-pink/src/main/resources/static/js/safeOptions.js
@@ -4,8 +4,8 @@ function showSafeOptions() {
 		$("p").show();
 	} else {
 		document.getElementById("safe").innerHTML = " I want a safe short Url";
-		document.getElementById("users").attribute = "select";
-		document.getElementById("time").attribute = "select";
+		document.getElementById("users").innerHTML = "Which users can access the url?";
+		document.getElementById("time").innerHTML = "How long do you want the url to be safe?";
 		$("p").hide();
 	}
 }
